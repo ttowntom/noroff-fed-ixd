9d597ac6360d8c361893d99d79129bfcc52c3797
ttowntom <117744552+ttowntom@users.noreply.github.com>
2023-11-22 13:26:22 +0100

    added sales tag to product cards

--- js/ui/home/createFeaturedCards.js
+++ js/ui/home/createFeaturedCards.js
@@ -16,24 +16,58 @@ export function createHomeFeaturedCards(favs, apiUrl) {
 		image.alt = favs[i].title;
 
 		let textSection = document.createElement("div");
-		textSection.classList.add("center", "flex", "flex--column");
+		textSection.classList.add(
+			"center",
+			"flex",
+			"flex--column",
+			"flex--space-around"
+		);
 
 		let title = document.createElement("h3");
 		title.innerText = favs[i].title;
 
-		let color = document.createElement("p");
-		color.innerText = favs[i].baseColor;
+		let price = document.createElement("div");
+		price.classList.add("product--card-price-container");
+		if (favs[i].discountedPrice === favs[i].price) {
+			let basePrice = document.createElement("p");
+			basePrice.classList.add(
+				"product--card-uppercase",
+				"product--card-single-line"
+			);
+			basePrice.innerText = "$" + favs[i].price;
+			price.appendChild(basePrice);
+		} else if (favs[i].discountedPrice < favs[i].price) {
+			let discount = document.createElement("p");
+			discount.classList.add(
+				"product--card-uppercase",
+				"product--card-on-sale"
+			);
+			discount.innerText = "$" + favs[i].discountedPrice;
+			price.appendChild(discount);
+
+			let basePrice = document.createElement("p");
+			basePrice.innerText = "$" + favs[i].price;
+			basePrice.classList.add("line-through");
+			price.appendChild(basePrice);
+		}
+
+		let bottomSection = document.createElement("div");
+		bottomSection.classList.add("width--100");
 
 		let hr = document.createElement("div");
 		hr.classList.add("hr");
 
 		let details = document.createElement("p");
+		details.classList.add("product--card-details");
+		details.classList.add("product--card-uppercase");
 		details.innerText = "See details";
 
+		bottomSection.appendChild(hr);
+		bottomSection.appendChild(details);
+
 		textSection.appendChild(title);
-		textSection.appendChild(color);
-		textSection.appendChild(hr);
-		textSection.appendChild(details);
+		textSection.appendChild(price);
+		textSection.appendChild(bottomSection);
 
 		card.appendChild(image);
 		card.appendChild(textSection);
